600a02f2aaed747f43f5ee8b5764207ce80cf240
wujiale-spec <1284698788@qq.com>
2021-10-14 16:50:57 +0800

    创建<Icon/>组件

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,16 @@
+const path = require('path')
+
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+  chainWebpack: config => {
+    const dir = path.resolve(__dirname, 'src/assets/icons')//确定icon所在的目录，__dirname就是当前目录
+
+    config.module//config是VUE封装的webpack对象
+      .rule('svg-sprite')
+      .test(/\.svg$/)
+      .include.add(dir).end() // 只包含 icons 目录，其它目录不走这个规则
+      .use('svg-sprite-loader').loader('svg-sprite-loader').options({extract: false}).end()//不解析文件
+    config.plugin('svg-sprite').use(require('svg-sprite-loader/plugin'), [{plainSprite: true}])//配置插件
+    config.module.rule('svg').exclude.add(dir) // 其他 svg loader 排除 icons 目录
+  }
 }
