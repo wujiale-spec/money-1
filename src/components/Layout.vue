edefc78b0435d7717e0d0a470f49cd9ad34e4237
wujiale-spec <1284698788@qq.com>
2021-10-15 17:31:31 +0800

    添加Nav样式

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -14,14 +14,12 @@ export default {
 </script>
 <style lang="scss" scoped>
 .nav-wrapper {
-  border: 1px solid green;
   display: flex;
   flex-direction: column;
   height: 100vh;
 }
 
 .content {
-  border: 1px solid blue;
   overflow: auto;
   flex-grow: 1;
 }
