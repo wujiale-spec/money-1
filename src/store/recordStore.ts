7ef36f8f22bc7d7091302cf287a602d825ddb39c
wujiale-spec <1284698788@qq.com>
2021-11-15 17:31:28 +0800

    将models整合到store里,删除models

--- src/store/recordStore.ts
+++ src/store/recordStore.ts
@@ -1,9 +1,25 @@
-import recordListModel from '@/models/RecordListModel';
+import clone from '@/lib/clone';
 
-export default {
-  //record store
-  recordList: recordListModel.fetch(),
-  createRecord: (record:RecordItem) => {
-    recordListModel.create(record);
+const localStorageKeyName = 'recordList';
+
+const recordStore = {
+  recordList: [] as RecordItem[],
+  fetchRecords() {
+    this.recordList = JSON.parse(window.localStorage.getItem(localStorageKeyName) || '[]') as RecordItem[];
+    return this.recordList;
+  },
+  saveRecords() {
+    window.localStorage.setItem(localStorageKeyName, JSON.stringify(this.recordList));
+  },
+  createRecord(record: RecordItem) {
+    const record2: RecordItem = clone(record);
+    record2.createdAt = new Date();
+    this.recordList && this.recordList.push(record2);
+    this.saveRecords();
   }
-}
+
+};
+
+recordStore.fetchRecords();
+
+export default recordStore;
