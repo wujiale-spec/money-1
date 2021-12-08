0b17cb4312d7516d486c7b9feaa8f795742b2634
wujiale-spec <1284698788@qq.com>
2021-12-08 12:58:58 +0800

    部署网站到money-website

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,9 @@
 const path = require('path')
 
 module.exports = {
+  publicPath: process.env.NODE_ENV==='production'
+      ? '/money-website/'
+      :'/',
   lintOnSave: false,
   chainWebpack: config => {
     const dir = path.resolve(__dirname, 'src/assets/icons')//确定icon所在的目录，__dirname就是当前目录
